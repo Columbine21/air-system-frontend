f2947488ed9cfe5becd62303755e1ea7caa29b17
Columbine21 <ziqiyuan@bupt.edu.cn>
2020-05-21 22:32:33 +0800

    finish setting

--- src/pages/admin/admin.vue
+++ src/pages/admin/admin.vue
@@ -27,6 +27,7 @@
         </el-menu-item>
       </el-menu>
     </el-aside>
+
     <el-container>
       <el-header style="font-size: 12px; height: 10vh">
 				<div class="header" style="height: 100%; text-align: right;">
@@ -42,16 +43,80 @@
           </el-dropdown>
         </div>
 			</el-header>
-      <el-main>
-        <el-divider direction="vertical"></el-divider>
-          <el-col :span="12">
-            sdjfaiojrga
-          </el-col>
-          
-          <el-col :span="12">
-            fahgaira
-          </el-col>
 
+      <el-main>
+        <div v-show="showControl.selectSettings">
+
+          <el-card style="margin:5vh 20%; width: 60%;">
+            <div slot="header">
+              <span>系统设置</span>
+              <el-button style="float: right; padding: 3px 0" type="text">应用</el-button>
+            </div>
+            <div style="margin-top: 4px; font-size: 13px">系统时间</div>
+            <el-date-picker
+              ref="date"
+              v-model="Settings.Date"
+              align="right"
+              type="date"
+              value-format="yyyy-MM-dd"
+              style="margin-top: 10px; margin-left: 30%"
+              placeholder="Please set the Date."
+              >
+            </el-date-picker>
+            <el-switch style="margin: 20px 0 0 40%"
+              v-model="Settings.State"
+              active-text="开启"
+              inactive-text="关闭">
+            </el-switch>
+          </el-card>
+
+          <el-card style="margin:5vh 20%; width: 60%;">
+            <div slot="header">
+              <span>中央空调状态</span>
+            </div>
+            <div>当前状态&ensp; :&ensp; {{Settings.State}}</div>
+            <div style="margin-top: 3vh">默认模式&ensp; :&ensp; {{Mode}}</div>
+            <div style="margin-top: 3vh">
+              <span>温度控制</span>
+              <el-slider style="margin: 0 1vw" v-model="Settings.temperature" :step='20' show-stops :marks="marksTemp">
+              </el-slider>
+            </div>
+            <div style="margin-top: 3vh">
+              <span>风速控制</span>
+              <el-slider style="margin: 0 1vw" v-model="Settings.wind" :step='50' show-stops :marks="marksWind">
+              </el-slider>
+            </div>
+          </el-card>
+
+          
+          
+        </div>
+        <div v-show="showControl.selectInspect">
+          <el-divider direction="vertical"></el-divider>
+          <el-col :span="12">
+            <el-card style="margin-left: 4vw; margin-top: 2vw; width: 80%;">
+              <div slot="header">
+                <span>从控机请求列表</span>
+                <el-button style="float: right; padding: 3px 0" type="text">Refresh</el-button>
+              </div>
+              <el-table :data="tableData" height="420" style="width: 100%" stripe border >
+                <el-table-column prop="roomId" label="房间号" width="110" /> 
+                <el-table-column prop="temperature" label="设定温度" width="110" /> 
+                <el-table-column prop="mode" label="设定风速" />
+              </el-table>
+              
+            </el-card>
+          </el-col>
+          <el-col :span="12">
+          </el-col>
+        </div>
+        <div v-show="showControl.selectStatistics">
+          <el-divider direction="vertical"></el-divider>
+          <el-col :span="12">
+          </el-col>
+          <el-col :span="12">
+          </el-col>
+        </div>
       </el-main>
     </el-container>
   </el-container>
@@ -62,19 +127,118 @@ export default {
   name: 'Admin',
   data () {
     return {
+      showControl: {
+        selectSettings: true,
+        selectInspect: false,
+        selectStatistics: false
+      },
       Manager: {
         avaterUrl: 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png',
         name: "Jason"
-      }
+      },
+      Settings: {
+        Date: '2020-5-20',
+        State: 'off',
+        temperature: null,
+        wind: null
+      },
+      marksWind: {
+        0: 'low',
+        50: 'medium',
+        100: 'high'
+      },
+      tableData: [{
+            roomId: '2-231',
+            temperature: '18',
+            mode: 'high'
+          }, {
+            roomId: '3-304',
+            temperature: '20',
+            mode: 'high'
+          }, {
+            roomId: '1-119',
+            temperature: '19',
+            mode: 'medium'
+          }, {
+            roomId: '1-119',
+            temperature: '19',
+            mode: 'medium'
+          }, {
+            roomId: '1-119',
+            temperature: '19',
+            mode: 'medium'
+          }, {
+            roomId: '1-119',
+            temperature: '19',
+            mode: 'medium'
+          }, {
+            roomId: '1-119',
+            temperature: '19',
+            mode: 'medium'
+          }, {
+            roomId: '1-119',
+            temperature: '19',
+            mode: 'medium'
+          }, {
+            roomId: '1-119',
+            temperature: '19',
+            mode: 'medium'
+          }
+      ]
     }
   },
   methods: {
     showChange(key, keyPath) {
       console.log(key + '...' + keyPath);
-      // this.$router.push('admin/' + key)
+      if (key == 'settings') {
+        this.showControl.selectSettings = true,
+        this.showControl.selectInspect = false,
+        this.showControl.selectStatistics = false
+      } else if (key == 'inspect') {
+        this.showControl.selectSettings = false,
+        this.showControl.selectInspect = true,
+        this.showControl.selectStatistics = false
+      } else {
+        this.showControl.selectSettings = false,
+        this.showControl.selectInspect = false,
+        this.showControl.selectStatistics = true
+      }
     },
     handleLogout () {
       this.$router.push('/')
+    }
+  },
+  computed: {
+    Season () {
+      return Number(this.Settings.Date.split('-')[1])
+    },
+    Mode () {
+      if (this.Season >= 5 && this.Season <= 10) {
+        return "制冷模式"
+      } else {
+        return "制热模式"
+      }
+    },
+    marksTemp () {
+      if (this.Season >= 5 && this.Season <= 10) {
+        return { 
+          0: '18°C',
+					20: '19°C',
+					40: '20°C',
+					60: '21°C',
+					80: '22°C',
+					100: '23°C'
+				}
+      } else {
+        return {
+          0: '26°C',
+					20: '27°C',
+					40: '28°C',
+					60: '29°C',
+					80: '30°C',
+					100: '31°C'
+        }
+      }
     }
   }
 }
@@ -100,4 +264,9 @@ export default {
     left: 58%;
     margin: 0;
   }
+
+  .el-picker-panel {
+    margin: 5px 50px
+  }
+
 </style>
--- src/pages/admin/subpages/setting.vue
+++ src/pages/admin/subpages/setting.vue
@@ -1,5 +1,75 @@
 <template>
-  <div>Settings menu</div>
+  <div>
+    <el-divider direction="vertical"></el-divider>
+      <el-col :span="12">
+        <el-card style="margin:8% 4vw; width: 80%;">
+          <div slot="header">
+            <span>系统设置</span>
+            <el-button style="float: right; padding: 3px 0" type="text">应用</el-button>
+          </div>
+          <div style="margin-top: 4px; font-size: 13px">系统时间</div>
+          <el-date-picker
+            ref="date"
+            v-model="Settings.Date"
+            align="right"
+            type="date"
+            value-format="yyyy-MM-dd"
+            style="margin-top: 10px; margin-left: 18%"
+            placeholder="Please set the Date."
+            >
+          </el-date-picker>
+          <el-switch style="margin: 20px 0 0 30%"
+            v-model="Settings.State"
+            active-text="开启"
+            inactive-text="关闭">
+          </el-switch>
+        </el-card>
+        <el-card style="margin:4vw; width: 80%;">
+          <div slot="header">
+            <span>中央空调状态</span>
+          </div>
+          <div>当前状态&ensp; :&ensp; {{Settings.State}}</div>
+          <div style="margin-top: 3vh">默认模式&ensp; :&ensp; {{Mode}}</div>
+          <div style="margin-top: 3vh">
+            <span>温度控制</span>
+            <el-slider style="margin: 0 1vw" v-model="Settings.temperature" :step='20' show-stops :marks="marksTemp">
+            </el-slider>
+          </div>
+          <div style="margin-top: 3vh">
+            <span>风速控制</span>
+            <el-slider style="margin: 0 1vw" v-model="Settings.wind" :step='50' show-stops :marks="marksWind">
+            </el-slider>
+          </div>
+        </el-card>
+      </el-col>
+      
+      <el-col :span="12">
+        <el-card style="margin-left: 4vw; margin-top: 2vw; width: 80%;">
+          <div slot="header">
+            <span>响应的从控机</span>
+          </div>
+          <el-table
+            :data="tableData"
+            max-height="200"
+            style="width: 100%">
+            <el-table-column
+              prop="roomId"
+              label="房间号"
+              width="120">
+            </el-table-column>
+            <el-table-column
+              prop="temperature"
+              label="设定温度"
+              width="120">
+            </el-table-column>
+            <el-table-column
+              prop="mode"
+              label="设定风速">
+            </el-table-column>
+          </el-table>
+        </el-card>
+      </el-col>
+  </div>
 </template>
 
 <script>
