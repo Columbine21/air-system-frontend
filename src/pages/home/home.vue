1b57783007a409edb23e32f2af691af7f3d3ebe1
Columbine21 <ziqiyuan@bupt.edu.cn>
2020-06-06 22:41:42 +0800

    Refactoring login page

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,47 +1,38 @@
 <template>
-  <div>
-    <img src="../../assets/bkPhoto.jpeg" class="bkPhoto" />
-    <div class="login">
-      <div class="header-wrapper">
-        <img class="header-img" src='../../assets/logo.png' />
-      </div>
-      <el-form ref="loginForm" :model="form" :rules="rules" class="login-box">
-        <div class="image-wrapper">
-          <img class="login-avater" src="../../assets/avater.jpeg" />
+  <el-container>
+    <img src="../../assets/bkPhot2.jpg" 
+    style=" z-index: -1; position: fixed; width: 100vw; height: 100vh;" />
+    
+    <el-card style="width: 40%; margin: 0 auto; margin-top: 15vh; margin-bottom: 10vh; opacity: .8">
+        <div>
+          <img style="margin: 0 20%;width: 60%" src='../../assets/logo.png' />
         </div>
-        <div class="field-box">
-          <div class="account">
-            <el-form-item label="账号" prop="username">
-              <el-input type="text" class='field' placeholder="请输入账号" v-model="form.username" />
-            </el-form-item>
-          </div>
-          <div class="password">
-            <el-form-item label="密码" prop="password">
-              <el-input type="password" class='field' placeholder="请输入密码" v-model="form.password" />
-            </el-form-item>
-          </div>
-          <div class="checkBox">
-            <el-checkbox v-model="isadmin">Admin</el-checkbox>
-          </div>
-          <div class="validity-check-info" v-show="!validity">
-              {{this.message}}
-          </div>
-          <div class="button-box">
-            <el-button class="loginButton" type="primary" v-on:click="onSubmit('loginForm')">登录</el-button>
-            <el-button class="signupButton" type="primary" v-on:click="onSubmit('signUpForm')">注册</el-button>
-          </div>
-        </div>
+        
+      <el-form ref="loginForm" :model="form" :rules="rules">
+        <img  style="width: 20%; margin:10px 40%" src="../../assets/avater.jpeg" />
+       
+          <el-form-item label="账号" prop="username">
+            <el-input type="text" class='field' placeholder="请输入账号" v-model="form.username" />
+          </el-form-item> 
+        
+          <el-form-item label="密码" prop="password">
+            <el-input type="password" class='field' placeholder="请输入密码" v-model="form.password" />
+          </el-form-item>
+          
+          <el-form-item label="用户类型" prop="userType" style="text-align: center">
+            <el-radio-group v-model="form.userType">
+              <el-radio label="Traveler" /> <el-radio label="Administrator" />
+            </el-radio-group>
+        </el-form-item>
+          
+          <el-button style="margin:0 20%; width: 60%;" type="primary" v-on:click="onSubmit('loginForm')">登录</el-button>
+          <el-button style="margin:5% 20%; width: 60%;" type="primary" v-on:click="onSubmit('signUpForm')">注册</el-button>
       </el-form>
-
-      <!-- <el-dialog title="温馨提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
-        <span>请输入账号和密码</span>
-        <span slot="footer" class="dialog-footer">
-          <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
-        </span>
-      </el-dialog> -->
-    </div>
-  </div>
+    </el-card>
+  </el-container>
+  <!-- </div> -->
 </template>
+
 <script>
 export default {
   name: 'Login',
@@ -51,7 +42,8 @@ export default {
       message: '',
       form: {
         username: '',
-        password: ''
+        password: '',
+        userType: ''
       },
       isadmin: false,
       // 表单验证，需要在 el-form-item 元素中增加 prop 属性
@@ -62,6 +54,9 @@ export default {
         password: [
           { required: true, message: '密码不可为空', trigger: 'blur' },
           {min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur'}
+        ], 
+        userType: [
+          {required: true, message: '用户类别不可为空'}
         ]
       },
       // 对话框显示和隐藏
@@ -73,7 +68,7 @@ export default {
       if (type === 'loginForm') {
         if (this.form.password && this.form.username) {
           // Todo : change it into axios post request
-          console.log('login \n' + this.form.username + '\n' + this.form.password)
+          console.log('login \n' + this.form.username + '\n' + this.form.password + '\n' + this.form.userType)
         } else {
           this.validity = false
           this.message = 'User Name or Password can not be null'
@@ -89,187 +84,4 @@ export default {
 
 <style lang="stylus" scoped>
 @import '~styles/variables.styl'
-.login {
-  display: flex;
-  flex-direction: column;
-  align-items: center;  /* block 垂直居中 */
-  justify-content: space-around;  /* block 水平居中 */
-  height: 100vh; /* 垂直居中的时候需要设定 height */
-}
-
-.header-wrapper {
-  height: 10vh;
-  align-content: center;
-}
-
-.header-img {
-  height: 100%;
-}
-
-.login-box {
-  width: 60vw;
-  height: 50vh;
-  margin-bottom: 10vh
-  background-color: white;
-  opacity: 0.85
-  border-radius: 5px;
-  -webkit-border-radius: 5px;
-  -moz-border-radius: 5px;
-}
-
-.image-wrapper {
-  text-align: center;
-  padding-top: 2.5vh;
-}
-
-.login-avater {
-  height: 5vh;
-  width: 10vh;
-}
-
-.label {
-  width: 10vw;
-  display: flex;
-  align-items: center;
-}
-
-.account {
-  padding-top: 2vh;
-  display: flex;
-}
-
-.password {
-  display: flex;
-}
-
-.checkBox {
-  width:79%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.checkBox >>> .el-checkbox {
-  font-size: 4px
-  align-items: center;
-  justify-content: center;
-}
-
-.checkBox >>> .el-checkbox__label {
-  line-height: 3vh;
-  font-size: 2.5vh;
-  // margin-top: 2vh
-}
-
-.checkBox >>> .el-checkbox__inner {
-  width: 3vh;
-  height: 3vh;
-  border-radius: 1vh;
-  border: .4vh solid #DCDFE6;
-}
-..checkBox >>> .el-checkbox__input {
-  height: 2vh
-  line-height: 2vh
-}
-
-.checkBox >>> .el-checkbox__inner::after{
-  height: 0;
-  width: 0;
-}
-
-.el-form-item {
-    margin-bottom: 2vh;
-}
-
-.field-box {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.field >>> .el-input__inner {
-  line-height: 5vh;
-  height: 5vh;
-  font-size: 1vh;
-  border-radius: 1vh;
-  border: .4vh solid #DCDFE6;
-  padding: 0 2vw;
-}
-
-.account >>> .el-form-item__content {
-  display: flex
-}
-
-.account >>> .el-form-item__label {
-  line-height: 5vh;
-  font-size: 2vh;
-  padding: 0 2vw 0 0;
-}
-.account >>> .el-form-item__error {
-  font-size: .2vh;
-  padding-top: .5vh;
-  line-height: 2.5vh;
-}
-
-.password >>> .el-form-item__label {
-  line-height: 5vh;
-  font-size: 2vh;
-  padding: 0 2vw 0 0;
-}
-
-.password >>> .el-form-item__content {
-  display: flex
-}
-
-.password >>> .el-form-item__error {
-  font-size: .2vh;
-  padding-top: .5vh;
-  line-height: 2.5vh;
-}
-
-.field {
-  width: 35vw;
-  line-height: 5vh
-  height: 5vh
-}
-
-.validity-check-info {
-  margin-top: .1rem
-  text-align: center
-  color: red;
-}
-
-.button-box {
-  width: 70%;
-  display : flex;
-  flex-direction : column;
-  align-items : center;
-  justify-content : center;
-}
-
-.button-box >>> .el-button {
-  padding: 0 20px
-  font-size: 2vw
-}
-
-.loginButton {
-  margin-top: 1vh;
-  height: 5vh;
-  width: 40vw;
-}
-
-.signupButton {
-  width: 40vw;
-  height: 5vh;
-  margin-left:0;
-  margin-top:1vh;
-}
-
-.bkPhoto {
-  z-index: $backgroundIndex;
-  position: fixed;
-  width: 100vw;
-  height: 100vh;
-}
 </style>
